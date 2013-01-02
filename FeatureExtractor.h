498e430018fa4df0ab92cfc8073efb0d90924287
André Mourão <a.mourao@campus.fct.unl.pt>
2013-01-02 13:42:45 +0000

    Changes to FeatureExtractor

--- FeatureExtractor.h
+++ FeatureExtractor.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+#include <vector>
 
 using namespace cv;
+using namespace std;
 
 class FeatureExtractor
 {
@@ -13,5 +17,24 @@ public:
 
 	virtual void extractFeatures(Mat& src, Mat& dst) = 0;
 	virtual int getFeatureVectorSize() = 0;
+
+	void extractFeatures(string& filename, vector<float>& features){
+		Mat src = imread(filename);
+		Mat dst;
+		extractFeatures(src,dst);
+		float* array = (float*)dst.data;
+		features = vector<float>(array, array + sizeof(float)*dst.cols*dst.rows);
+	}
+
+	void extractFeatures(vector<float>& src, vector<float>& dst){
+		Mat srcMat(src,true);
+		srcMat = srcMat.reshape(1,1);
+		Mat dstMat;
+		extractFeatures(srcMat,dstMat);
+		float* array = (float*)dstMat.data;
+		dst = vector<float>(array, array + sizeof(float)*dstMat.cols*dstMat.rows);
+	}
+
+
 };
 
