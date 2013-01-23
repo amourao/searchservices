babae1280a5ac31a60dd0136251b4703fce91d45
gtavares <gtavares22@gmail.com>
2013-01-23 15:55:54 +0000

    Added in GameClass support for features

--- appData/GameImage.cpp
+++ appData/GameImage.cpp
@@ -2,11 +2,11 @@
 
 const string SQLFILE = "slb.db";
 
-GameImage::GameImage(unsigned int pk, int gameId,int roundId,int userId,
+GameImage::GameImage(string url,int gameId,int roundId,int userId,
 		int timeId, int roundAudience, 	int roundExpressionId, int ksvm, float score,
-		string username)
+		string username, NVector gaborFace, NVector histFace, NVector reconFace, NVector gaborShirt,
+		NVector histShirt, NVector reconShirt) : IDataModel(url)
 {
-	this->pk = pk;
 	this->gameId = gameId;
 	this->roundId = roundId;
 	this->userId = userId;
@@ -16,30 +16,58 @@ GameImage::GameImage(unsigned int pk, int gameId,int roundId,int userId,
 	this->ksvm = ksvm;
 	this->score = score;
 	this->username = username;
+	this->gaborFace = gaborFace;
+	this->histFace = histFace;
+	this->reconFace = reconFace;
+	this->gaborShirt = gaborShirt;
+	this->histShirt = histShirt;
+	this->reconShirt = reconShirt;
 }
 
-GameImage::GameImage(){}
+GameImage::GameImage() : IDataModel()
+{
+	gaborFace = *(new NVector("FACE_GABOR"));
+	histFace = *(new NVector("FACE_HIST"));
+	reconFace = *(new NVector("FACE_RECON"));
+	gaborShirt = *(new NVector("SHIRT_GABOR"));
+	histShirt = *(new NVector("SHIRT_HIST"));
+	reconShirt = *(new NVector("SHIRT_RECON"));
+}
 
 GameImage::~GameImage(){
 
 }
 
-bool GameImage::storeSQL(){
+bool GameImage::storeSQL()
+{
+	int mediaId = getMediaId();
 	Session ses("SQLite", SQLFILE);
-	ses << "INSERT INTO gameImage VALUES(:pk,:gameId,:roundId,:userId,:timeId,:roundAudience,:roundExpressionId,:ksvm,:score,:username)",
-			Keywords::use(pk),Keywords::use(gameId),Keywords::use(roundId), Keywords::use(userId), Keywords::use(timeId),Keywords::use(roundAudience),
+	ses << "INSERT INTO gameImage VALUES(:mediaId,:gameId,:roundId,:userId,:timeId,:roundAudience,:roundExpressionId,:ksvm,:score,:username)",
+			Keywords::use(mediaId),Keywords::use(gameId),Keywords::use(roundId), Keywords::use(userId), Keywords::use(timeId),Keywords::use(roundAudience),
 			Keywords::use(roundExpressionId), Keywords::use(ksvm), Keywords::use(score), Keywords::use(username), Keywords::now;
+	gaborFace.storeSQL();
+	histFace.storeSQL();
+	reconFace.storeSQL();
+	gaborShirt.storeSQL();
+	histShirt.storeSQL();
+	reconShirt.storeSQL();
 	ses.close();
 	return true;
 }
 
-bool GameImage::loadSQL(int pk){
+bool GameImage::loadSQL(int mediaId)
+{
 	Session ses("SQLite", SQLFILE);
-	this->pk = pk;
-	ses << "SELECT * FROM gameImage WHERE pk="<<pk,
-			Keywords::into(pk), Keywords::into(gameId),Keywords::into(roundId),Keywords::into(userId),Keywords::into(timeId),Keywords::into(roundAudience),
+	ses << "SELECT * FROM gameImage WHERE pk="<<mediaId,
+			Keywords::into(mediaId), Keywords::into(gameId),Keywords::into(roundId),Keywords::into(userId),Keywords::into(timeId),Keywords::into(roundAudience),
 			Keywords::into(roundExpressionId), Keywords::into(ksvm), Keywords::into(score), Keywords::into(username), Keywords::now;
 	ses.close();
+	gaborFace.loadSQL(mediaId);
+	histFace.loadSQL(mediaId);
+	reconFace.loadSQL(mediaId);
+	gaborShirt.loadSQL(mediaId);
+	histShirt.loadSQL(mediaId);
+	reconShirt.loadSQL(mediaId);
 	return true;
 }
 
@@ -53,10 +81,6 @@ int GameImage::getGameId(){
 
 int GameImage::getKsvm() {
 	return ksvm;
-}
-
-unsigned int GameImage::getPk() {
-	return pk;
 }
 
 int GameImage::getRoundAudience(){
