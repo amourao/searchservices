e9b8a5e0641d8311d7c3372185480090a672a446
Andre Mourao <a.mourao@campus.fct.unl.pt>
2014-04-27 02:56:11 +0100

    Adaped GenericIndexer endpoint for multiple images

--- rest/Endpoints/indexer/GenericIndexer.cpp
+++ rest/Endpoints/indexer/GenericIndexer.cpp
@@ -62,68 +62,74 @@ void GenericIndexer::handleRequest(string method, map<string, string> queryStrin
 string GenericIndexer::retrieve(map<string, string> parameters){
 	FileDownloader fd;
 
+    vector<string> filenames = StringTools::split(parameters["url"],',');
+    string analyserName = parameters["analyser"];
+    string indexerName = parameters["indexer"];
+    string taskName = parameters["task"];
+    string retrievalType = parameters["type"];
+    string outputFormat = parameters["output"];
+    int n = atoi(parameters["n"].c_str());
 
-	string filename = fd.getFile(parameters["url"]);
-	string analyserName = parameters["analyser"];
-	string indexerName = parameters["indexer"];
-	string taskName = parameters["task"];
-	string retrievalType = parameters["type"];
-	string outputFormat = parameters["output"];
-	int n = atoi(parameters["n"].c_str());
+    FactoryIndexer * fc = FactoryIndexer::getInstance();
+    IIndexer* indexer = (IIndexer*)fc->createType(indexerName);
 
+    stringstream ss;
+    ss << taskName << "_" << analyserName << "_" << indexerName;
+    indexer->load(ss.str());
 
-	FactoryAnalyser * f = FactoryAnalyser::getInstance();
+    FactoryAnalyser * f = FactoryAnalyser::getInstance();
+    IAnalyser* analyser= (IAnalyser*)f->createType(analyserName);
 
-	IAnalyser* analyser= (IAnalyser*)f->createType(analyserName);
+    vector<std::pair< vector<string>, vector<float> > > resultLists;
 
-	IDataModel* data = analyser->getFeatures(filename);
-	vector<float>* features = (vector<float>*) data->getValue();
+    for (uint ii = 0; ii < filenames.size(); ii++){
+        string filename = fd.getFile(filenames.at(ii));
+        IDataModel* data = analyser->getFeatures(filename);
+        vector<float>* features = (vector<float>*) data->getValue();
+        std::pair< vector<string>, vector<float> > resultList;
 
-	FactoryIndexer * fc = FactoryIndexer::getInstance();
+        if (retrievalType == "normal"){
+            resultList = indexer->knnSearchName(*features,n);
+        } else if (retrievalType == "radius"){
+            float radius = atof(parameters["radius"].c_str());
+            resultList = indexer->radiusSearchName(*features,radius,n);
+        }
+        resultLists.push_back(resultList);
 
-	IIndexer* indexer = (IIndexer*)fc->createType(indexerName);
-
-	stringstream ss;
-
-	ss << taskName << "_" << analyserName << "_" << indexerName;
-
-	indexer->load(ss.str());
-
-	std::pair< vector<string>, vector<float> > resultList;
-
-	if (retrievalType == "normal"){
-		resultList = indexer->knnSearchName(*features,n);
-	} else if (retrievalType == "radius"){
-		float radius = atof(parameters["radius"].c_str());
-		resultList = indexer->radiusSearchName(*features,radius,n);
-	}
-
-
+    }
 	if(outputFormat == "json"){
 
 		Json::Value root;
 		Json::Value results;
 
-		Json::Value featureArray(Json::arrayValue);
-		Json::Value distArray(Json::arrayValue);
-		for (int i = 0; i < resultList.first.size(); i++){
-			featureArray.append(resultList.first.at(i));
-			distArray.append(resultList.second.at(i));
+        Json::Value images(Json::arrayValue);
+		for (int j = 0; j < resultLists.size(); j++){
+            Json::Value featureArray(Json::arrayValue);
+            Json::Value distArray(Json::arrayValue);
+            std::pair< vector<string>, vector<float> > resultList = resultLists.at(j);
+            for (int i = 0; i < resultList.first.size(); i++){
+                featureArray.append(resultList.first.at(i));
+                distArray.append(resultList.second.at(i));
+			}
+			Json::Value pairArray;
+			pairArray["filename"] = filenames.at(j);
+			pairArray["n_true"] = (int)resultList.first.size();
+            pairArray["idList"] = featureArray;
+            pairArray["distList"] = distArray;
+            images.append(pairArray);
 		}
 
 		root["result"] = "ok";
+		root["results"] = images;
 		root["indexer"] = ss.str();
-		root["filename"] = filename;
 		root["analyser"] = parameters["analyser"];
 		root["indexer"] = parameters["indexer"];
 		root["task"] = parameters["task"];
 		root["type"] = parameters["type"];
 		root["n"] = atoi(parameters["n"].c_str());
-		root["n_true"] = (int)resultList.first.size();
 		if (retrievalType == "radius")
-			root["n"] = atof(parameters["radius"].c_str());
-		root["idList"] = featureArray;
-		root["distList"] = distArray;
+            root["n"] = atoi(parameters["radius"].c_str());
+
 
 		stringstream ssJ;
 		ssJ << root;
@@ -131,9 +137,12 @@ string GenericIndexer::retrieve(map<string, string> parameters){
 	} if(outputFormat == "trec"){
 		stringstream ssJ;
 
-		for (int i = 0; i < resultList.first.size(); i++){
-			ssJ << "0\t1\t" << resultList.first.at(i) << "\t" << (i+1) << "\t" << "\t" << resultList.second.at(i) << "\t" << taskName << endl;
-		}
+        for (int j = 0; j < resultLists.size(); j++){
+            std::pair< vector<string>, vector<float> > resultList = resultLists.at(j);
+            for (int i = 0; i < resultList.first.size(); i++){
+                ssJ << j << "\t1\t" << resultList.first.at(i) << "\t" << (i+1) << "\t" << "\t" << resultList.second.at(i) << "\t" << taskName << endl;
+            }
+        }
 		return ssJ.str();
 	}
 
